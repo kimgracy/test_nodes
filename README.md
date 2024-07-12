109f58b523c89b2e9b218a45e36d5f2fa1d86159
okj <okj001010@gmail.com>
2024-07-13 07:43:54 +0900

    FIX: repo bug & mc_test_02 home

--- README.md
+++ README.md
@@ -1,35 +1,43 @@
 # PX4-Autopilot
-대회에 사용할 PX4-Autopilot **v1.15.0-beta2**를 설치해야 한다.   
+
+대회에 사용할 PX4-Autopilot **v1.15.0-beta2**를 설치해야 한다.  
 기존의 PX4-Autopilot에서 바로 checkout 해도 되지만, 혹시 모를 issue에 대비해서 그냥 지웠다가 다시 까는 것을 추천한다. ([reference](https://docs.px4.io/main/en/contribute/git_examples.html#get-a-specific-release))
+
 1. 기존 PX4-Autopilot 제거
+
    ```
    cd
    rm -rf PX4-Autopilot/
    ```
 
 2. PX4-Autopilot clone
+
    ```
    git clone https://github.com/PX4/PX4-Autopilot.git
    cd PX4-Autopilot/
    ```
 
 3. version 변경
+
    ```
    git checkout v1.15.0-beta2
    ```
 
 4. submodules update (오래 걸림)
+
    ```
    make submodulesclean
    ```
 
-5. Ubuntu 환경 setup & 컴퓨터 재부팅   
+5. Ubuntu 환경 setup & 컴퓨터 재부팅  
    (보통 안 해도 잘 되긴 하는데 재부팅 안 했다고 오류 찾느라 고생한 적이 있다. 혹시 모르니 아래 명령어 실행 후 그냥 재부팅을 하도록 하자.)
+
    ```
    bash ./Tools/setup/ubuntu.sh
    ```
 
 6. build (오래 걸림)
+
    ```
    cd PX4-Autopilot/
    make px4_sitl gazebo-classic
@@ -40,27 +48,29 @@
 8. standard_vtol도 download 받은 후 압축 풀어서 PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models 경로에 덮어씌우기
 
 9. 대회 환경 build
+
    ```
    cd PX4-Autopilot/
    make px4_sitl gazebo-classic_standard_vtol__bulnabi
    ```
 
-10. PX4-Autopilot/src/modules/uxrce_dds_client/dds_topics.yaml 에서 아래 내용들을 각각 publications와 subscriptions 항목의 가장 마지막 부분에 추가   
-      ```
-      - topic: /fmu/out/vtol_vehicle_status
-      type: px4_msgs::msg::VtolVehicleStatus
-      ```
-      ```
-      - topic: /fmu/in/gimbal_manager_set_manual_control
-      type: px4_msgs::msg::GimbalManagerSetManualControl
-      ```
-
+10. PX4-Autopilot/src/modules/uxrce_dds_client/dds_topics.yaml 에서 아래 내용들을 각각 publications와 subscriptions 항목의 가장 마지막 부분에 추가
+    ```
+    - topic: /fmu/out/vtol_vehicle_status
+    type: px4_msgs::msg::VtolVehicleStatus
+    ```
+    ```
+    - topic: /fmu/in/gimbal_manager_set_manual_control
+    type: px4_msgs::msg::GimbalManagerSetManualControl
+    ```
 
 # Installation
-1. 해당 repo fork   
+
+1. 해당 repo fork  
    scroll을 위로 올려보면 Edit pins / Watch / Fork / Star 라고 적힌 부분이 있는데 Fork를 클릭하고 Create Fork를 눌러주자.
 
 2. workspace 만들기
+
    ```
    cd
    mkdir test_ws
@@ -68,20 +78,25 @@
    ```
 
 3. 본인 repository를 보면 fork된 repo가 생겼을텐데, 해당 repo clone
+
    ```
    git clone https://github.com/[your_github_id]/test_nodes.git
    ```
 
 4. src 폴더로 이름 변경
+
    ```
    mv test_nodes src
    ```
 
 5. directory 구조 확인
+
    ```
    tree -aL 2
    ```
+
    (정상적인 출력 결과)
+
    ```
    └── src
        ├── .git
@@ -95,24 +110,29 @@
    ```
 
 6. upstream 등록
+
    ```
+   cd src
    git remote add upstream https://github.com/Bulnabi-SNU/test_nodes.git
    ```
 
 7. git 연결 확인
+
    ```
    git remote -v
    ```
+
    (정상적인 출력 결과)
+
    ```
     origin	https://github.com/[your_github_id]/test_nodes.git (fetch)
     origin	https://github.com/[your_github_id]/test_nodes.git (push)
     upstream	https://github.com/Bulnabi-SNU/test_nodes.git (fetch)
     upstream	https://github.com/Bulnabi-SNU/test_nodes.git (push)
-    ```
+   ```
 
-8. (Optional) alias 추가   
-   나는 잘 안 하는 편인데 원하는 사람은 .bashrc에 ```alias rosfoxy='source ~/test_ws/install/local_setup.bash'```와 같이 추가해도 좋다.
+8. (Optional) alias 추가  
+   나는 잘 안 하는 편인데 원하는 사람은 .bashrc에 `alias rosfoxy='source ~/test_ws/install/local_setup.bash'`와 같이 추가해도 좋다.
 
 # Requirements
 
@@ -122,24 +142,19 @@
    sudo apt update
    sudo apt install ros-${ROS_DISTRO}-v4l2-camera
    ```
-<br/>
 
-2. Change the path of local yolov5
+   <br/>
 
-   Download YOLOv5: https://github.com/ultralytics/yolov5
+2. Download yolov5 at home directory
 
-   modify '/test_ws/src/yolo_detection/yolo_detection/yolo_detector.py'
-
-   '/home/chaewon/yolov5' -> path of 'yolov5' in your computer
-   
    ```
-    # define model path and load the model
-        model_path = os.path.join(os.getcwd(), 'src/yolo_detection/config/best.pt')
-        self.model = torch.hub.load('/home/chaewon/yolov5', 'custom', path=model_path, source='local')
+   cd
+   git clone https://github.com/ultralytics/yolov5
    ```
-<br/>
 
-3. (Optional) v4l2-ctl: change v4l2_camera settings   
+   <br/>
+
+3. (Optional) v4l2-ctl: change v4l2_camera settings  
    (WSL을 사용하는 경우에는 안 되기 때문에 pass.. 혹시라도 되면 알려주면 감사 ㅜ)
 
    ```
@@ -150,7 +165,10 @@
    ```
 
 # Build
-my_bboxes_msg 관련 dependency 문제는 해결해 놨으니, 그냥 아래에 적힌 대로 colcon build 한번만 실행해도 된다. (해당 repo에 아직 auto landing 관련 코드는 없기 때문에 auto landing은 dependency 잘 고려해서 build 문제 없도록 코드 올리기!)
+
+- 아래와 같이 colcon build 한번만 실행해도 되는걸로 변경했는데 잘 돌아가는지 채원아 확인 부탁...
+- 해당 repo에 아직 auto landing 관련 코드는 없기 때문에 auto landing은 dependency 잘 고려해서 build 문제 없도록 코드 올리기!
+
 ```
 cd test_ws
 colcon build --symlink-install                  // cba
@@ -158,7 +176,9 @@ source ./install/local_setup.bash
 ```
 
 # Development
+
 ### Warning
+
 - px4_msgs는 별도로 clone 받을 필요 없이 해당 repo에 있는 것 기준으로 코드를 작성하면 된다.
 - sitl과 달리 실제 비행에서는 기체의 시작 위치의 local position이 origin이 아니다. 따라서 특정 WP로 이동하려면 takeoff 할 때 home position의 위치를 설정한 후, 상대적인 위치를 계산해야 한다. (test_02에서 self.home_position 관련 코드 확인)
 - 코드를 실행하더라도 기체가 바로 동작하지 않고, RC에서 스위치를 켜서 허용해야지 동작하도록 코드를 작성해야 한다. RC에서 offboard mode로 변환하는 스위치가 있기 때문에 offboard mode인지 확인 후 takeoff 하는 코드를 추가하면 된다. (test_02 takeoff_and_arm_callback 관련 코드 확인)
@@ -166,31 +186,39 @@ source ./install/local_setup.bash
 - 비행 시험의 편의성을 위해서 여러 개의 node를 run 해야 하는 경우에는 하나의 launch file로 만드는 것을 추천한다.
 
 ### Code Upload
-1. README.md 수정   
+
+1. README.md 수정  
    본인의 test 코드 실행 명령어 및 간략한 설명을 아래의 Run 부분에 추가
 
-2. 수정 사항을 본인 repo에 올리기
+2. upstream 변경 사항 반영
+   ```
+   git pull upstream main
+   ```
+3. 충돌 문제가 발생한다면, 충돌 문제 해결 후 병
+
+4. 수정 사항을 본인 repo에 올리기
+
    ```
    git add .
-   git commit -m "[YOUR_COMMIT_MESSAGE]" # e.g. ADD: yolo_test_00
+   git commit -m "[YOUR_COMMIT_MESSAGE]" # e.g. FIX: yolo_test_00 gimbal control
    git push origin main
    ```
 
-3. 본인 repo에 들어가서 commit이 제대로 됐는지 클릭해서 확인하고 Compare & Pull reqeust 버튼을 클릭한다. 본인이 어떤 코드를 작성했는지 **자세하게** 설명하고 reviewer에 okj001010을 추가 후, Create pull request 버튼을 클릭한다.
+5. 본인 repo에 들어가서 commit이 제대로 됐는지 클릭해서 확인하고 Compare & Pull reqeust 버튼을 클릭한다. 본인이 어떤 코드를 작성했는지 **자세하게** 설명하고 reviewer에 okj001010을 추가 후, Create pull request 버튼을 클릭한다.
 
-4. 여기까지 한 이후에는 코드 확인 후 직접 merge 할 예정이니, (아마 안 되긴 할텐데) **절대** 바로 merge 버튼을 누르지 말아야 한다.
-
+6. 여기까지 한 이후에는 코드 확인 후 직접 merge 할 예정이니, (아마 안 되긴 할텐데) **절대** 바로 merge 버튼을 누르지 말아야 한다.
 
 # Run
 
 ```
-ros2 run test_nodes test_00
-ros2 run test_nodes test_01
-ros2 run test_nodes test_02 --ros-args --params-file ~/test_ws/src/test_nodes/config/test_02_waypoint.yaml
+ros2 run test_nodes mc_test_00
+ros2 run test_nodes mc_test_01
+ros2 run test_nodes mc_test_02 --ros-args --params-file ~/test_ws/src/vehicle_controller/config/mc_test_02_waypoint.yaml
 ```
-- test_00: arming
-- test_01: takeoff → land
-- test_02: takeoff → N 2m → NE 2m → E 2m → home → land (사각 비행)
+
+- mc_test_00: arming
+- mc_test_01: takeoff → land
+- mc_test_02: takeoff → N 2m → NE 2m → E 2m → home → land (사각 비행)
 
 <br/>
 <br/>
---
+++ vehicle_controller/test_nodes/mc_test_02_mc_square.py
@@ -0,0 +1,233 @@
+__author__ = "Juyong Shin"
+__contact__ = "juyong3393@snu.ac.kr"
+
+# import rclpy
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+
+# import px4_msgs
+"""msgs for subscription"""
+from px4_msgs.msg import VehicleStatus
+from px4_msgs.msg import VehicleLocalPosition
+"""msgs for publishing"""
+from px4_msgs.msg import VehicleCommand
+from px4_msgs.msg import OffboardControlMode
+from px4_msgs.msg import TrajectorySetpoint
+
+# import math, numpy
+import math
+import numpy as np
+
+class VehicleController(Node):
+
+    def __init__(self):
+        super().__init__('vehicle_controller')
+
+        """
+        0. Configure QoS profile for publishing and subscribing
+        """
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        """
+        1. Constants
+        """
+        self.mc_acceptance_radius = 0.3
+        self.acceptance_heading_angle = np.radians(0.5)
+
+        """
+        2. Set waypoints
+        """
+        self.WP = [np.array([0.0, 0.0, 0.0])]
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('WP1', None),
+                ('WP2', None),
+                ('WP3', None),
+                ('WP4', None),
+            ])
+
+        for i in range(1, 5):
+            wp_position = self.get_parameter(f'WP{i}').value
+            self.WP.append(np.array(wp_position))
+
+        """
+        3. State variables
+        """
+        # phase description
+        # -2 : after flight
+        # -1 : before flight
+        # 0 : takeoff and arm
+        # i >= 1 : moving toward WP_i
+        self.phase = -1
+        self.vehicle_status = VehicleStatus()
+        self.vehicle_local_position = VehicleLocalPosition()
+        self.home_position = np.array([0.0, 0.0, 0.0])
+        self.pos = np.array([0.0, 0.0, 0.0])
+        
+        self.previous_goal = None
+        self.current_goal = None
+
+        """
+        4. Create Subscribers
+        """
+        self.vehicle_status_subscriber = self.create_subscription(
+            VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile
+        )
+        self.vehicle_local_position_subscriber = self.create_subscription(
+            VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile
+        )
+
+        """
+        5. Create Publishers
+        """
+        self.vehicle_command_publisher = self.create_publisher(
+            VehicleCommand, '/fmu/in/vehicle_command', qos_profile
+        )
+        self.offboard_control_mode_publisher = self.create_publisher(
+            OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile
+        )
+        self.trajectory_setpoint_publisher = self.create_publisher(
+            TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile
+        )
+
+        """
+        6. timer setup
+        """
+        self.offboard_heartbeat = self.create_timer(0.1, self.offboard_heartbeat_callback)
+        self.takeoff_timer = self.create_timer(0.5, self.takeoff_and_arm_callback)
+        self.main_timer = self.create_timer(0.5, self.main_timer_callback)
+        
+        print("Successfully executed: vehicle_controller")
+        print("Please switch to offboard mode.")
+        
+    
+    """
+    Services
+    """   
+    def land(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+        self.phase = -2
+
+    """
+    Callback functions for the timers
+    """
+    def offboard_heartbeat_callback(self):
+        """offboard heartbeat signal"""
+        self.publish_offboard_control_mode(position=True)
+
+    def takeoff_and_arm_callback(self):
+        if self.phase == -1 and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
+            print("Takeoff and arm")
+            # takeoff and arm only if the vehicle is in offboard mode by RC switch
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_TAKEOFF)
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
+            self.home_position = self.pos # set home position
+            self.phase = 0
+    
+    def main_timer_callback(self):
+        if self.phase == 0:
+            if self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_AUTO_LOITER:
+                self.publish_vehicle_command(
+                    VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 
+                    param1=1.0, # main mode
+                    param2=6.0  # offboard
+                )
+                self.phase = 0.5
+        elif self.phase == 0.5:
+            self.current_goal = self.WP[1]
+            self.publish_trajectory_setpoint(position_sp=self.current_goal)
+            self.phase = 1
+        elif self.phase >= 1:
+            distance = np.linalg.norm(self.pos - self.current_goal)
+            if distance < self.mc_acceptance_radius:
+                if self.phase == 4:
+                    self.land()
+                    self.phase = -2
+                else:
+                    self.previous_goal = self.current_goal
+                    self.current_goal = self.WP[self.phase + 1]
+                    self.publish_trajectory_setpoint(position_sp=self.current_goal)
+                    self.phase += 1
+            else:
+                self.publish_trajectory_setpoint(position_sp=self.current_goal)
+        print(self.phase)
+
+    """
+    Callback functions for subscribers.
+    """        
+    def vehicle_status_callback(self, msg):
+        """Callback function for vehicle_status topic subscriber."""
+        self.vehicle_status = msg
+    
+    def vehicle_local_position_callback(self, msg):
+        self.vehicle_local_position = msg
+        self.pos = np.array([msg.x, msg.y, msg.z])
+        if self.phase != -1:
+            # set position relative to the home position after takeoff
+            self.pos = self.pos - self.home_position
+
+    """
+    Functions for publishing topics.
+    """
+    def publish_vehicle_command(self, command, **kwargs):
+        """Publish a vehicle command."""
+        msg = VehicleCommand()
+        msg.command = command
+        msg.param1 = kwargs.get("param1", float('nan'))
+        msg.param2 = kwargs.get("param2", float('nan'))
+        msg.param3 = kwargs.get("param3", float('nan'))
+        msg.param4 = kwargs.get("param4", float('nan'))
+        msg.param5 = kwargs.get("param5", float('nan'))
+        msg.param6 = kwargs.get("param6", float('nan'))
+        msg.param7 = kwargs.get("param7", float('nan'))
+        msg.target_system = 1
+        msg.target_component = 1
+        msg.source_system = 1
+        msg.source_component = 1
+        msg.from_external = True
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.vehicle_command_publisher.publish(msg)
+    
+    def publish_offboard_control_mode(self, **kwargs):
+        msg = OffboardControlMode()
+        msg.position = kwargs.get("position", False)
+        msg.velocity = kwargs.get("velocity", False)
+        msg.acceleration = kwargs.get("acceleration", False)
+        msg.attitude = kwargs.get("attitude", False)
+        msg.body_rate = kwargs.get("body_rate", False)
+        msg.thrust_and_torque = kwargs.get("thrust_and_torque", False)
+        msg.direct_actuator = kwargs.get("direct_actuator", False)
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.offboard_control_mode_publisher.publish(msg)
+    
+    def publish_trajectory_setpoint(self, **kwargs):
+        msg = TrajectorySetpoint()
+        # position setpoint is relative to the home position
+        msg.position = list( kwargs.get("position_sp", np.nan * np.zeros(3)) + self.home_position )
+        msg.velocity = list( kwargs.get("velocity_sp", np.nan * np.zeros(3)) )
+        msg.yaw = kwargs.get("yaw_sp", float('nan'))
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.trajectory_setpoint_publisher.publish(msg)
+        # self.get_logger().info(f"Publishing position setpoints {setposition}")
+    
+def main(args = None):
+    rclpy.init(args=args)
+
+    vehicle_controller = VehicleController()
+    rclpy.spin(vehicle_controller)
+
+    vehicle_controller.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
